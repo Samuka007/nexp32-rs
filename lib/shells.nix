f93b7c1d06447da88ed67f89910270d2d8196c2d
Samuka007 <24754678@student.uwa.edu.au>
2026-04-01 08:43:15 +0000

    feat: add probe-rs-tools to common packages and clean up PATH exports

--- lib/shells.nix
+++ lib/shells.nix
@@ -23,6 +23,7 @@ let
     rust-analyzer
     esp-generate
     espflash
+    probe-rs-tools
   ];
 
   makeShellHook = target: extraEnv: ''
@@ -37,15 +38,15 @@ let
     echo "ESP tools:"
     echo "  - esp-generate --chip <esp32|esp32s2|esp32s3> <project-name>"
     echo ""
-    export PATH="${complete}/bin:${xtensa-esp32-elf}/bin:$HOME/.cargo/bin:$PATH"
+    export PATH="${complete}/bin:${xtensa-esp32-elf}/bin:$PATH"
     export RUST_SRC_PATH="${complete}/lib/rustlib/src/rust/library"
     export RUSTC_SYSROOT="${complete}"
-    
+
     # Configure GCC as the linker for Xtensa targets
     export CARGO_TARGET_XTENSA_ESP32_NONE_ELF_LINKER="xtensa-esp32-elf-gcc"
     export CARGO_TARGET_XTENSA_ESP32S2_NONE_ELF_LINKER="xtensa-esp32-elf-gcc"
     export CARGO_TARGET_XTENSA_ESP32S3_NONE_ELF_LINKER="xtensa-esp32-elf-gcc"
-    
+
     ${extraEnv}
   '';
 
